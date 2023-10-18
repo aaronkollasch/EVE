60ca7cc8e1b162ebbd5cb39a6f168afe466873ba
Aaron Kollasch <aaron@kollasch.dev>
2023-10-18 15:38:36 -0400

    Add tier2 to export_alignment_results_pg

--- scripts/export_alignment_results_pg.py
+++ scripts/export_alignment_results_pg.py
@@ -54,14 +54,37 @@ if tier == 1:
     assert len(df["protein"].unique()) == len(df)
     prio_df = df
     run_names = [
-        "clinical_subs_need_models_uniref30_2202_c40",
+        "proteingym_colabfold_to_run_uniref30_2202_c50",
     ]
     prio_df["protein_index"] = -1
     with open("colabfold_to_run.fasta") as f:
         for i, (name, seq) in enumerate(SimpleFastaParser(f)):
             name = name.split()[0]
             prio_df.loc[prio_df["protein"] == name, "protein_index"] = i
-    assert (prio_df["protein_index"] != -1).all(), prio_df[prio_df["protein_index"] == -1]
+    prio_df = prio_df[(prio_df["protein_index"] != -1) & (prio_df["msa_location"].str.contains("_colabfold"))]
+    print(len(df), len(prio_df))
+    print(prio_df)
+
+if tier == 2:
+    project_name = "proteingym"
+    df = pd.read_csv("clinical_subs_need_weights_2023_10_17_EVE.csv")
+    df.rename(columns={"protein_name": "protein"}, inplace=True)
+    assert len(df["protein"].unique()) == len(df)
+    prio_df = df
+    run_names = [
+        "proteingym_clinical_subs_uniref30_2202_c50_20231018",
+    ]
+    prio_df["protein_index"] = -1
+    with open("clinical_subs_need_weights_2023_10_18.fasta") as f:
+        for i, (name, seq) in enumerate(SimpleFastaParser(f)):
+            name = name.split()[0]
+            prio_df.loc[prio_df["protein"] == name, "protein_index"] = i
+    prio_df = prio_df[(prio_df["protein_index"] != -1) & (prio_df["msa_location"].str.contains("_colabfold"))]
+    print(len(df), len(prio_df))
+    print(prio_df)
+
+for name in run_names:
+    os.makedirs(f"/tmp/{name}/", exist_ok=True)
 
 def make_a2ms(run_name, project_name, tup):
     i, msa_location, protein = tup
@@ -72,7 +95,7 @@ def make_a2ms(run_name, project_name, tup):
     error = Exception()
     while run < max_runs:
         try:
-            subprocess.run(["aws", "s3", "cp", f"s3://markslab-us-east-2/colabfold/output/{project_name}/MSA/{run_name}/{i}.a3m", f"/tmp/{run_name}/{msa_location}.a3m"], check=True)
+            subprocess.run(["aws", "s3", "cp", f"s3://markslab-us-east-2/colabfold/output/{project_name}/{run_name}/{i}.a3m", f"/tmp/{run_name}/{msa_location}.a3m"], check=True)
         except subprocess.CalledProcessError as e:
             error = e
             continue
@@ -88,7 +111,7 @@ def make_a2ms(run_name, project_name, tup):
     run = 0
     while run < max_runs:
         try:
-            subprocess.run(["aws", "s3", "cp", f"/tmp/{run_name}/{protein}.a2m", f"s3://markslab-private/eve/{project_name}/data/MSA/{run_name}/{msa_location}.a2m"], check=True)
+            subprocess.run(["aws", "s3", "cp", f"/tmp/{run_name}/{msa_location}.a2m", f"s3://markslab-private/eve/{project_name}/data/MSA/{run_name}/{msa_location}.a2m"], check=True)
         except subprocess.CalledProcessError as e:
             error = e
             continue
@@ -106,9 +129,10 @@ for run_name in run_names:
 
     def get_run_files():
         for row in prio_df.itertuples():
-            yield row.protein_index, row.protein
+            yield row.protein_index, row.msa_location, row.protein
 
     p = pool.Pool(len(os.sched_getaffinity(0)) * 2)
+    #p = pool.Pool(1)
     i = tqdm(
         p.imap(
             partial(make_a2ms, run_name, project_name),
